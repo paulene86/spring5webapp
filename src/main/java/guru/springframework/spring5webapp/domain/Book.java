5837b9be2ccf3bcec0bb7f505fc8fabed5a6f71e
SALAZARO GUERRA <paulo.cesar.salazaro.guerra.ext@ing.com>
2020-05-11 16:13:14 +0200

    Added publisher changes

--- src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -1,9 +1,7 @@
 package guru.springframework.spring5webapp.domain;
 
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
